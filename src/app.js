b44785f29d2fe26d43d145feb721a9b3715874f7
codesby-raviteja <raviteja555@gmail.com>
2025-03-18 14:13:02 +0530

    explore Routing in Express js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,43 @@ const express = require("express")
 
 const app = express()
 
-app.use("/test", (req, res) => res.send("HELLO FROM TEST ROUTE"))
 
-app.use((req, res) => {
-  res.send("Hello from serveesr")
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Raviteja",
+    lastname: "Yellam",
+  })
 })
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully sent to Database")
+})
+
+app.delete("/user", (req, res) => {
+  res.send("Data successfully deleted")
+})
+
+app.patch("/user", (req, res) => {
+  res.send("Data successfully patched")
+})
+
+app.use("/hell", (req, res) => res.send("HELLO 2 ROUTE"))
+
+app.use("/hello", (req, res) => res.send("HELLO HELLO  ROUTE"))
+app.use("/test/45", (req, res) => res.send("HELLO FROM FIRST TEST ROUTE"))
+
+app.use("/test", (req, res) => {
+  res.send("HELLO FROM TEST ROUTE")
+})
+
+app.use("/user", (req, res) => {
+  res.send("USER IS OUR DATA")
+})
+
+
+// app.use("/", (req, res) => {
+//   res.send("Hello from server")
+// })
 
 app.listen(4000, () => {
   console.log("sucessfully initiated server")
