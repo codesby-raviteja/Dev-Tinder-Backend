4f6e936171a040d734332a9e886f5d5a9f64e803
codesby-raviteja <raviteja555@gmail.com>
2025-03-22 19:26:39 +0530

    Rest API's

--- src/app.js
+++ src/app.js
@@ -4,23 +4,120 @@ const User = require("./Models/user")
 
 const app = express()
 
-app.post("/signup", async (req, res) => {
-  //Creating a new Instance of User model
+//to convert/parse the request Object into Js Object
+app.use(express.json())
 
-  const user = new User({
-    firstName: "Jyothi",
-    lastName: "Yellam",
-    emailId: "jyothiyellam@email.com",
-    password: "jyothi@321",
-    age: 25,
-    gender: "Female",
-  })
+//Signingup the user
+app.post("/signup", async (req, res) => {
+  const userData = req.body
+
+  //Creating a new Instance of User model
+  const user = new User(userData)
 
   try {
     await user.save()
     res.send("User successfully signed up")
   } catch (err) {
-    res.statusCode(400).send("Error in singin up the user" + err.message)
+    res.status(400).send("Error in singin up the user" + err.message)
+  }
+})
+
+//Get user by emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId
+
+  try {
+    const user = await User.findOne({ emailId: userEmail })
+    if (!user) {
+      res.send("user not found.")
+    } else {
+      res.send(user)
+    }
+  } catch (err) {
+    res.status(404).send("Error in getting user data.")
+  }
+})
+
+//Feed Api to get all the users of matching emailId,firstName,etc...
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId
+
+  try {
+    const users = await User.find({ emailId: userEmail })
+    if (!users.length > 0) {
+      //  res.send(users)
+      res.send("No user found")
+    } else {
+      res.send(users)
+    }
+  } catch (err) {
+    res.status(400).sent
+  }
+})
+
+//get user by Id
+app.get("/getuserbyId", async (req, res) => {
+  const userId = req.body.userId
+
+  try {
+    const user = await User.findById(userId)
+    console.log(user)
+    if (!user) {
+      res.send("No user found")
+    } else {
+      res.send(user)
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(404).send("Error in getting user data.")
+  }
+})
+
+//Delete the user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId
+
+  try {
+    const a = await User.findByIdAndDelete(userId)
+    console.log(a)
+    res.send("user successfully deleted")
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+//Update the user by Id
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId
+  const updateObj = req.body
+
+  try {
+    // const a = await User.findByIdAndUpdate(userId, updateObj)
+    const a = await User.findByIdAndUpdate({ _id: userId }, updateObj)
+
+    //  by default returned the previous document (before update)
+    // console.log(a)
+
+    res.send("user successfully updated")
+  } catch (err) {
+    res.status(404).send("Something went wrong.")
+  }
+})
+
+//Update the user by Email id
+app.patch("/updateuserbyemailld", async (req, res) => {
+  const userEmail = req.body.emailId
+  const userObj = req.body
+
+  try {
+    const a = await User.findOneAndUpdate({ emailId: userEmail }, userObj, {
+      returnDocument: "after",
+      lean: true,
+    })
+    console.log(a)
+    res.send("user updated successfully")
+  } catch (err) {
+    res.status(404).send("Something went wrong.")
   }
 })
 
